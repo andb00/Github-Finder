e9b2a3ef141a4cb2f6094f6ea8550c2f13182f85
andrew <andrewbetancourt00@gmail.com>
2024-06-28 00:59:42 -0700

    Initial Commit

---
+++ Github Finder/Github_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_FinderApp.swift
+//  Github Finder
+//
+//  Created by Andrew Betancourt on 6/28/24.
+//
+
+import SwiftUI
+
+@main
+struct Github_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            LoginView(prompt: "")
+        }
+    }
+}
